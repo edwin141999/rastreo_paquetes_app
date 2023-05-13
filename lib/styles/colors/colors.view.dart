1c2be0a4bb6c3f3b4fbd094e67cff6bfaf25cdba
edwin141999 <gendevilman@gmail.com>
2023-05-13 13:53:40 -0600

    colors view

---
+++ lib/styles/colors/colors.view.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/animation.dart';
+
+class ColorSelect {
+  static const kColorPrimary = Color(0xffFF4B3A);
+  static const kColorSecondary = Color(0xff3C77BC);
+  static const kWhite = Color(0xffFFFFFF);
+  static const kGrayLight = Color(0xff777E90);
+  static const kBlack = Color(0xff000000);
+}
