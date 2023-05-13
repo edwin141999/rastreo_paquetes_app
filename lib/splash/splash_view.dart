116548daadd6e2ee2d4a8ae0fe7fa0e89ddafb6d
edwin141999 <gendevilman@gmail.com>
2023-05-13 13:53:29 -0600

    splash screen done

---
+++ lib/splash/splash_view.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:rastreo_paquetes_app/styles/colors/colors.view.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({Key? key}) : super(key: key);
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+    _toOnboarding();
+  }
+
+  _toOnboarding() async {
+    await Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/inicio');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        height: double.infinity,
+        width: double.infinity,
+        child: Stack(
+          children: [
+            Image.asset(
+              'assets/background.png',
+              fit: BoxFit.cover,
+            ),
+            Center(
+              child: Image.asset(
+                'assets/logo.png',
+                fit: BoxFit.cover,
+                color: ColorSelect.kWhite.withOpacity(.80),
+                colorBlendMode: BlendMode.modulate,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
